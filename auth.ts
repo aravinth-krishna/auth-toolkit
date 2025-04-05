0cd9338b2986110059ce66e2342abec2509d98e9
Aravinth krishna <r.aravinthkrishna@gmail.com>
2025-04-05 22:24:46 +0530

    Fixed type issue

--- auth.ts
+++ auth.ts
@@ -4,17 +4,46 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
 import authConfig from "@/auth.config";
+import { getUserById } from "./data/user";
+import { Role } from "@prisma/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
+    // async signIn({ user }) {
+    //   if (user.id) {
+    //     const existingUser = await getUserById(user.id);
+
+    //     if (!existingUser || !existingUser.emailVerified) {
+    //       return false;
+    //     }
+    //   }
+
+    //   return true;
+    // },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
+      if (token.role && session.user) {
+        session.user.role = token.role as Role;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
