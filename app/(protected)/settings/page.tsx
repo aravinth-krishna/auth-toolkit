80336d4ce515e9ed1421d35cbc64de7b11aecfef
Aravinth krishna <r.aravinthkrishna@gmail.com>
2025-04-08 16:08:23 +0530

    Added Name updation field in settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,29 +1,85 @@
 "use client";
 
+import * as z from "zod";
 import { settings } from "@/actions/settings";
 import { useSession } from "next-auth/react";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { SettingsSchema } from "@/schemas";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { BsCheckCircle, BsExclamationTriangle } from "react-icons/bs";
 
 const SettingsPage = () => {
+  const user = useCurrentUser();
+
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
   const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: user?.name || undefined,
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
-      settings({
-        name: "Infamous Blue Wire",
-      }).then(() => {
-        update();
-      });
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+          }
+
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+          }
+        })
+        .catch(() => {
+          setError("Something went wrong");
+        });
     });
   };
 
   return (
     <div>
       <h2>Settings</h2>
-      <button onClick={onClick} disabled={isPending}>
-        Update Name
-      </button>
+      <form onSubmit={form.handleSubmit(handleSubmit)} noValidate>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            disabled={isPending}
+            type="text"
+            placeholder="John Doe"
+            {...form.register("name")}
+          />
+          {form.formState.errors.name && (
+            <p>{form.formState.errors.name.message}</p>
+          )}
+        </div>
+        {error ? (
+          <p>
+            <BsExclamationTriangle /> {error}
+          </p>
+        ) : (
+          ""
+        )}
+        {success ? (
+          <p>
+            <BsCheckCircle /> {success}
+          </p>
+        ) : (
+          ""
+        )}
+        <button type="submit" disabled={isPending}>
+          Save
+        </button>
+      </form>
     </div>
   );
 };
